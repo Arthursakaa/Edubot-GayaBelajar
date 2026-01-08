e84e396418cd4469700da190a70ceb2c098c5dcd
Arthur Saka <arthursakaa16@gmail.com>
2026-01-08 21:20:04 +0800

    fix database connection

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,7 @@ const ML_URL = process.env.ML_URL;
 // ======================
 // DATABASE
 // ======================
-const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
+const db = mysql.createConnection(process.env.MYSQL_URL);
 
 db.connect(err => {
   if (err) {
